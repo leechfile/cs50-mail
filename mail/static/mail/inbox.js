b9dc42df86dd47c483173764c3324c436746e387
leechfile <2544048170@qq.com>
2023-12-07 08:55:17 +0800

    1

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
-
+  document.querySelector('input[type="submit"]').addEventListener('click',send_email)
   // By default, load the inbox
   load_mailbox('inbox');
 });
@@ -20,6 +20,7 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+
 }
 
 function load_mailbox(mailbox) {
@@ -31,17 +32,45 @@ function load_mailbox(mailbox) {
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
+  // send / inbox /  archive
 
 }
+
+function send_email(){
+
+  // get the content
+  recipients = document.querySelector('#compose-recipients').value
+  subject = document.querySelector('#compose-subject').value
+  body = document.querySelector('#compose-body').value
+
+  // send the post requests
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+        recipients: recipients,
+        subject: subject,
+        body: body
+    })
+  })
+  // return message
+  .then(response => response.json())
+  .then(result => {
+      console.log(result);
+  });
+}
+
+
+
+
+/*
 function load_inbox(){
   // load all email send requests
   fetch('/emails/inbox')
   .then(response => response.json())
   .then(emails => {
       box = document.querySelector('#email-form');
-      emails.forEach(email){
+      emails.forEach(email)
 
-    }
       box.appendChild()
 
 
@@ -50,4 +79,4 @@ function load_inbox(){
   // render the HTML page append li
 
 }
-
+*/
